30ca9ab96d7ad7b233c1a3944ddc69da54d2115a
Kartikeya Kandpal <104270854+kartikeyyy03@users.noreply.github.com>
2023-06-04 18:53:16 +0530

    Add files via upload

---
+++ question_2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class question_2{
+
+public static void main(String[] args) {
+
+Scanner sc=new Scanner(System.in);
+
+int N=sc.nextInt();
+
+if(N<1|N>1000000){System.out.println("Invalid input");}
+else{
+    
+    int i=1;
+    while(i<=N){
+        System.out.println(i);
+        i=i+2;
+          
+
+    
+        
+    }
+
+    
+
+    
+    }
+    
+}
+
+}
